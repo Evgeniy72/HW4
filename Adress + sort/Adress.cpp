ca47f9a40dd1c1e24743382951f3d2abc18a62bf
Evgeniy <trior_tmn@list.ru>
2022-10-09 15:15:36 +0500

    first commit

---
+++ Adress + sort/Adress.cpp
@@ -0,0 +1,84 @@
+﻿#include <fstream>
+#include <iostream>
+class Adress
+{
+private:
+	std::string C, H;
+	int nh, nr;
+public:
+		std::string output_adress() {
+		std::string ou = this->C + ", " + this->H + ", ";
+		return ou;
+	}
+		std::string a;
+		void get_C() { a = C;}
+	Adress()
+	{
+		C = "Неизвесный";
+		H = "Неизвестный";
+		nh = 1;
+		nr = 1;
+	}
+	Adress(std::string C, std::string H, int nh, int nr)
+	{
+		this->C = C;this->H = H; this->nh = nh; this->nr = nr;
+	}
+};
+void sort(Adress* adresses, int size) {
+		bool ch;
+	do
+	{
+		ch = false;
+		for (int i = 0; i < size-1; i++)
+		{
+			Adress ch1, ch2;
+			 ch1 = adresses[i];
+			 ch2 = adresses[i+1];
+			ch1.get_C();
+			ch2.get_C();
+			if (ch1.a > ch2.a)
+			{
+				Adress temp = adresses[i + 1];
+				adresses[i + 1] = adresses[i];
+				adresses[i] = temp;
+				ch = true;
+			}
+		}
+	} while (ch);
+	}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::ifstream fin("in.txt");
+	int q;
+	if (!(fin.is_open()))
+	{
+		std::cout << "Файл не найден";
+	}
+	else {
+		fin >> q;
+		Adress* arr = new Adress[q];
+		for (int i = 0; i < q; i++) {
+			std::string C, H;
+			int nh, nr;
+			fin >> C;
+			fin >> H;
+			fin >> nh;
+			fin >> nr;
+			Adress r(C, H, nh, nr);
+			arr[i] = r;
+		}
+		sort(arr, q);
+		std::ofstream fout("out.txt");
+		fout << q << std::endl;
+		for (int i = 0; i < q; i++) {
+			Adress r;
+			r = arr[i];
+			fout << r.output_adress() << std::endl;
+		}
+		delete[] arr;
+		fout.close();
+		fin.close();
+	}
+
+}
---
+++ Adress/Adress.cpp
@@ -0,0 +1,62 @@
+﻿#include <fstream>
+#include <iostream>
+class Adress
+{
+private:
+	std::string C, H;
+	int nh, nr;
+public:
+	std::string output_adress() {
+		char nhh, nrh;
+		nhh = nh;
+		nrh = nr;
+		std::string ou = this->C + ", " + this->H + ", " + nhh;
+		return ou;
+	}
+	Adress()
+	{
+		C = "Неизвесный";
+		H = "Неизвестный";
+		nh = 1;
+		nr = 1;
+	}
+	Adress(std::string C, std::string H, int nh, int nr)
+	{
+		this->C = C;this->H = H; this->nh = nh; this->nr = nr;
+	}
+};
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::ifstream fin("in.txt");
+	int q;
+	if (!(fin.is_open()))
+	{
+		std::cout << "Файл не найден";
+	}
+	else {
+		fin >> q;
+		Adress* arr = new Adress[q];
+		for (int i = 0; i < q; i++) {
+			std::string C, H;
+			int nh, nr;
+			fin >> C;
+			fin >> H;
+			fin >> nh;
+			fin >> nr;
+			Adress r(C, H, nh, nr);
+			arr[i] = r;
+		}
+		std::ofstream fout("out.txt");
+		fout << q << std::endl;
+		for (int i = q - 1; i >= 0; i--) {
+			Adress r;
+			r = arr[i];
+			fout << r.output_adress() << std::endl;
+		}
+		delete[] arr;
+		fout.close();
+		fin.close();
+	}
+
+}
